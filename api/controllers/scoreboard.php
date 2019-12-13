9ac63865057b268b12c4f73634f19ee245a7ab87
Sam Bernal <sambernaldamasco@gmail.com>
2019-12-13 17:41:28 -0500

    basic routing done

---
+++ api/controllers/scoreboard.php
@@ -0,0 +1,26 @@
+<?php
+// ------------ DEPENDENCIES
+header('Content-Type: application/json');
+include_once __DIR__ . '/../models/scoreboard.php';
+
+// ------------ REQUEST ROUTING
+switch ($_REQUEST['action']){
+  case "index":
+    echo "it me index";
+  break;
+
+  case "post":
+    echo "it me posting some stuff";
+  break;
+
+  case "update":
+    echo "updating all the stuff on id ";
+  break;
+
+  case "delete":
+    echo "deleting all the stuff on id ";
+  break;
+}
+
+
+?>
