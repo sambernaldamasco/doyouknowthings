a86ccd906052d9a0ac635bcffa0ae85a537cd1e4
Sam Bernal <sambernaldamasco@gmail.com>
2019-12-13 23:14:16 -0500

    randomizing answers + answer validation working

--- doyouknowthings/src/components/main.js
+++ doyouknowthings/src/components/main.js
@@ -4,12 +4,41 @@ import Question from './question.js'
 import Scoreboard from './scoreboard.js'
 
 class Main extends React.Component{
+  constructor(props){
+      super(props)
+      this.state = {
+
+      }
+  }
+
+  getQuestion = () => {
+      fetch('https://opentdb.com/api.php?amount=1')
+      .then(response => response.json())
+      .then(json => {
+          this.setState({
+              questionInfo:json.results[0],
+              answersArray: [json.results[0].correct_answer, ...json.results[0].incorrect_answers].sort(() => Math.random() - 0.5),
+              correctAnswer: json.results[0].correct_answer
+          })
+      }).catch(error => console.log(error))
+  }
+
     render(){
         return(
             <div className='main-component'>
                 this is the main component
                 <Form />
-                <Question />
+
+                <button onClick={()=>{this.getQuestion()}}>Get Random Question</button>
+                {
+                  this.state.questionInfo ?
+                  <Question
+                    questionInfo={this.state.questionInfo}
+                    answersArray={this.state.answersArray}
+                    correctAnswer={this.state.correctAnswer}
+                  />
+                  : null
+                }
                 <Scoreboard />
             </div>
         )
