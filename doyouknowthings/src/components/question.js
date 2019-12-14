3718e39dfd4e37ee4c2fe44081f75e58730e644e
Ayla Ex <aylaex01@gmail.com>
2019-12-13 19:51:30 -0500

    created function to retrieve random question from trivia api

--- doyouknowthings/src/components/question.js
+++ doyouknowthings/src/components/question.js
@@ -1,10 +1,40 @@
 import React from 'react'
 
 class Question extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+        
+        }
+    }
+
+    getQuestion = () => {
+        fetch('https://opentdb.com/api.php?amount=1')
+        .then(response => response.json())
+        .then(json => {
+            this.setState({
+                questionInfo:json
+            })
+        }).catch(error => console.log(error))
+    }
+
     render(){
         return(
             <div className='question-component'>
-                this is the question component
+                this is the question component <br />
+                <button onClick={()=>{this.getQuestion()}}>Get Random Question</button>
+                {
+                 (this.state.questionInfo) ?
+                 <div>
+                 Question: {this.state.questionInfo.results[0].question}<br/>
+                 Category: {this.state.questionInfo.results[0].category}<br/>
+                 Difficulty:
+                 {this.state.questionInfo.results[0].difficulty}<br/>
+
+
+                 </div>
+                 : null
+                }
             </div>
         )
     }
