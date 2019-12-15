30451e3e86768f342341ba82bdfc569076e7d000
Ayla Ex <aylaex01@gmail.com>
2019-12-14 23:16:00 -0500

    user can change the view with nav buttons to show or hide the scoreboard

--- doyouknowthings/src/components/main.js
+++ doyouknowthings/src/components/main.js
@@ -20,17 +20,23 @@ class Main extends React.Component{
   render(){
     return(
       <div className='main-component'>
-        {
-          this.state.playerName
-          ? <> <h1>Player - {this.state.playerName}</h1>
-          <Question playerName={this.state.playerName} startNewGame={this.startNewGame}/>
-          </>
+          {
+            this.state.playerName
+            ? <> <h1>Player - {this.state.playerName}</h1>
+            <Question playerName={this.state.playerName} startNewGame={this.startNewGame}/>
+            </>
+            :
+            <Form startNewGame={this.startNewGame}/>
+          }
+          {
+              (this.props.view === 'scoreboard') ?
+              <Scoreboard />
+              :
+              <div className='display-none'>
+              <Scoreboard />
+              </div>
+          }
 
-          :
-          <Form startNewGame={this.startNewGame}/>
-        }
-
-        <Scoreboard />
       </div>
     )
   }
