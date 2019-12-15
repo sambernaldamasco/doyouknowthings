7491c44e41872884e88bcc96365d9ce21ed79394
Ayla Ex <aylaex01@gmail.com>
2019-12-14 21:49:31 -0500

    rendered scoreboard in scoreboard component

--- doyouknowthings/src/components/scoreboard.js
+++ doyouknowthings/src/components/scoreboard.js
@@ -1,10 +1,72 @@
+// dependencies ===============
 import React from 'react'
 
+// database connection ===============
+let baseURL = '';
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:8888'
+} else {
+  console.log('this is for heroku');
+}
+
 class Scoreboard extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    componentDidMount(){
+        this.getScoreboard()
+    }
+
+    getScoreboard = () => {
+        fetch(`${baseURL}/scoreboard`)
+        .then(response => response.json())
+        .then(json => {
+            this.setState({
+                scoreboard: json
+            })
+        }).catch(error => console.log(error))
+    }
+
     render(){
         return(
             <div className='scoreboard-component'>
-                this is the scoreboard component
+                <table>
+                <thead>
+                    <tr>
+                    <th colSpan="2">SCOREBOARD</th>
+                    </tr>
+                    <tr>
+                        <th>NAME</th>
+                        <th>SCORE</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {
+                    // eventually this if statement should be changed to if the view === 'scoreboard'
+                    (this.state.scoreboard) ?
+                    this.state.scoreboard.map((score) => {
+                        return(
+                        <tr key={score.id}>
+                        <td className='scoreboard-name'>
+                        {score.name}
+                        </td>
+                        <td className='scoreboard-score'>
+                        {score.score}
+                        </td>
+                        </tr>
+                    )
+                    })
+                    : null
+                }
+                </tbody>
+                </table>
+
+
+
             </div>
         )
     }
